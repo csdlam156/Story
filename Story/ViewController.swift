17b2b8fb508d278214f49d84c804c74fd225b46b
bigdog156 <39177789+bigdog156@users.noreply.github.com>
2019-08-05 16:17:32 +0700

    Initial Commit

---
+++ Story/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Story
+//
+//  Created by Lê Thạch Lâm on 8/5/19.
+//  Copyright © 2019 Lê Thạch Lâm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
